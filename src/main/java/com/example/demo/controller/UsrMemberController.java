d3c317d6ee8d66fc61e99d2c7b547262a1a88381
es1210 <dmstn27034@gmail.com>
2024-11-08 00:11:51 +0900

    로그인 아이디 중복체크

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -1,20 +1,30 @@
 package com.example.demo.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.example.demo.service.MemberService;
 import com.example.demo.vo.Member;
+
 @Controller
 public class UsrMemberController {
+
 	@Autowired
 	private MemberService memberService;
+
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+		if (id == -1) {
+			return "이미 사용중인 아이디";
+		}
 		Member member = memberService.getMemberById(id);
 		return member;
 	}
+
 }
