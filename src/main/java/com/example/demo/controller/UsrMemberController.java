005c65a0dd37a52a513cc4046e8143d7e106d984
es1210 <dmstn27034@gmail.com>
2024-11-08 00:19:15 +0900

    입력된 데이터의 null체크 함수 구현

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -16,14 +16,36 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
+		
+		if(loginId == null || loginId.trim().length() == 0) {
+			return "loginId를 입력해주세요";
+		}
+		if(loginPw == null || loginPw.trim().length() == 0) {
+			return "loginPw를 입력해주세요";
+		}
+		if(name == null || name.trim().length() == 0) {
+			return "name를 입력해주세요";
+		}
+		if(nickname == null || nickname.trim().length() == 0) {
+			return "nickname를 입력해주세요";
+		}
+		if(cellphoneNum == null || cellphoneNum.trim().length() == 0) {
+			return "cellphoneNum를 입력해주세요";
+		}
+		if(email == null || email.trim().length() == 0) {
+			return "email를 입력해주세요";
+		}
+
 		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+
 		if (id == -1) {
 			return "이미 사용중인 아이디";
 		}
+
 		Member member = memberService.getMemberById(id);
+
 		return member;
 	}
 
