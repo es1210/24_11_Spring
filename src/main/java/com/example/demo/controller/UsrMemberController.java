bed2c84f5fb055ccdb24b0bff978d16bc3ec484e
es1210 <dmstn27034@gmail.com>
2024-11-11 13:39:15 +0900

    로그인 기능 구현

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -10,33 +10,60 @@ import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 import com.example.demo.vo.ResultData;
 
+import jakarta.servlet.http.HttpSession;
 @Controller
 public class UsrMemberController {
 
 	@Autowired
 	private MemberService memberService;
 
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData<Member> doLogin(HttpSession httpSession, String loginId, String loginPw) {
+		boolean isLogined = false;
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		if (isLogined) {
+			return ResultData.from("F-A", "이미 로그인 함");
+		}
+		if (Ut.isEmptyOrNull(loginId)) {
+			return ResultData.from("F-1", "loginId 입력 x");
+		}
+		if (Ut.isEmptyOrNull(loginPw)) {
+			return ResultData.from("F-2", "loginPw 입력 x");
+		}
+		Member member = memberService.getMemberByLoginId(loginId);
+		if (member == null) {
+			return ResultData.from("F-3", Ut.f("%s는(은) 존재 x", loginId));
+		}
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", Ut.f("비밀번호 틀림"));
+		}
+		httpSession.setAttribute("loginedMemberId", member.getId());
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다", member.getNickname()));
+	}
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
-		
-		if(loginId == null || loginId.trim().length() == 0) {
+
+		if (Ut.isEmptyOrNull(loginId)) {
 			return ResultData.from("F-1", "loginId 입력 x");
 		}
-		if(loginPw == null || loginPw.trim().length() == 0) {
+		if (Ut.isEmptyOrNull(loginPw)) {
 			return ResultData.from("F-2", "loginPw 입력 x");
 		}
-		if(name == null || name.trim().length() == 0) {
+		if (Ut.isEmptyOrNull(name)) {
 			return ResultData.from("F-3", "name 입력 x");
 		}
-		if(nickname == null || nickname.trim().length() == 0) {
+		if (Ut.isEmptyOrNull(nickname)) {
 			return ResultData.from("F-4", "nickname 입력 x");
 		}
-		if(cellphoneNum == null || cellphoneNum.trim().length() == 0) {
+		if (Ut.isEmptyOrNull(cellphoneNum)) {
 			return ResultData.from("F-5", "cellphoneNum 입력 x");
 		}
-		if(email == null || email.trim().length() == 0) {
+		if (Ut.isEmptyOrNull(email)) {
 			return ResultData.from("F-6", "email 입력 x");
 		}
 
@@ -45,7 +72,7 @@ public class UsrMemberController {
 		if (doJoinRd.isFail()) {
 			return doJoinRd;
 		}
-		
+
 		Member member = memberService.getMemberById((int) doJoinRd.getData1());
 
 		return ResultData.newData(doJoinRd, member);
